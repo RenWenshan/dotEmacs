dcf00b29b3529460aa809a5399bbb2044c4c5f03
Wenshan Ren <renws1990@gmail.com>
2012-04-06 09:57:38 +1000

    auto completion for python

--- init.el
+++ init.el
@@ -50,21 +50,128 @@
 
 ;; ---- Nicer end ----
 
+;; ---- Code Reading ----
+(require 'xcscope)
+(require 'ecb)
+
+(require 'highlight-symbol)
+
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-prev)
+
+;; ---- Code Reading end ----
 
 ;; ---- Programming ----
-;; Yasnippet
-(add-to-list 'load-path "~/.emacs.d/site-lisp/")
-(require 'yasnippet-bundle)
 
 ;; == Python ==
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+(progn (cd "~/.emacs.d/site-lisp")
+	(normal-top-level-add-subdirs-to-load-path))
+(require 'python)
+(require 'auto-complete)
+(require 'yasnippet)
 
-(load-file "emacs-for-python/epy-init.el")
-;; (add-to-list 'load-path "~/.emacs.d/site-lisp/python-mode")
-;; (setq py-install-directory "~/.emacs.d/site-lisp/python-mode")
-;; (require 'python-mode)
-;; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
-;; (require 'auto-complete)
+;; Initialize Pymacs                                                                                           
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+;; Initialize Rope                                                                                             
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+;; Initialize Yasnippet                                                                                        
+;Don't map TAB to yasnippet                                                                                    
+;In fact, set it to something we'll never use because                                                          
+;we'll only ever trigger it indirectly.                                                                        
+(setq yas/trigger-key (kbd "C-c <kp-multiply>"))
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/site-lisp/yasnippet-0.5.9/snippets")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
+;;; Auto-completion                                                                                            
+;;;  Integrates:                                                                                               
+;;;   1) Rope                                                                                                  
+;;;   2) Yasnippet                                                                                             
+;;;   all with AutoComplete.el                                                                                 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
+(defun prefix-list-elements (list prefix)
+  (let (value)
+    (nreverse
+     (dolist (element list value)
+      (setq value (cons (format "%s%s" prefix element) value))))))
+(defvar ac-source-rope
+  '((candidates
+     . (lambda ()
+         (prefix-list-elements (rope-completions) ac-target))))
+  "Source for Rope")
+(defun ac-python-find ()
+  "Python `ac-find-function'."
+  (require 'thingatpt)
+  (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
+    (if (null symbol)
+        (if (string= "." (buffer-substring (- (point) 1) (point)))
+            (point)
+          nil)
+      symbol)))
+(defun ac-python-candidate ()
+  "Python `ac-candidates-function'"
+  (let (candidates)
+    (dolist (source ac-sources)
+      (if (symbolp source)
+          (setq source (symbol-value source)))
+      (let* ((ac-limit (or (cdr-safe (assq 'limit source)) ac-limit))
+             (requires (cdr-safe (assq 'requires source)))
+             cand)
+        (if (or (null requires)
+                (>= (length ac-target) requires))
+            (setq cand
+                  (delq nil
+                        (mapcar (lambda (candidate)
+                                  (propertize candidate 'source source))
+                                (funcall (cdr (assq 'candidates source)))))))
+        (if (and (> ac-limit 1)
+                 (> (length cand) ac-limit))
+            (setcdr (nthcdr (1- ac-limit) cand) nil))
+        (setq candidates (append candidates cand))))
+    (delete-dups candidates)))
+(add-hook 'python-mode-hook
+          (lambda ()
+                 (auto-complete-mode 1)
+                 (set (make-local-variable 'ac-sources)
+                      (append ac-sources '(ac-source-rope) '(ac-source-yasnippet)))
+                 (set (make-local-variable 'ac-find-function) 'ac-python-find)
+                 (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
+                 (set (make-local-variable 'ac-auto-start) nil)))
+
+;;Ryan's python specific tab completion                                                                        
+(defun ryan-python-tab ()
+  ; Try the following:                                                                                         
+  ; 1) Do a yasnippet expansion                                                                                
+  ; 2) Do a Rope code completion                                                                               
+  ; 3) Do an indent                                                                                            
+  (interactive)
+  (if (eql (ac-start) 0)
+      (indent-for-tab-command)))
+
+(defadvice ac-start (before advice-turn-on-auto-start activate)
+  (set (make-local-variable 'ac-auto-start) t))
+(defadvice ac-cleanup (after advice-turn-off-auto-start activate)
+  (set (make-local-variable 'ac-auto-start) nil))
+
+(define-key python-mode-map "\t" 'ryan-python-tab)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
+;;; End Auto Completion                                                                                        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Electric Pairs
 (add-hook 'python-mode-hook
@@ -127,15 +234,3 @@
 
 
 
-;; ---- Code Reading ----
-(require 'xcscope)
-(require 'ecb)
-
-(require 'highlight-symbol)
-
-(global-set-key [(control f3)] 'highlight-symbol-at-point)
-(global-set-key [f3] 'highlight-symbol-next)
-(global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-prev)
-
-;; ---- Code Reading end ----
