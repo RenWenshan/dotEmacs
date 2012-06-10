52f777e27aee40fa9f03965f30103c7ab09b4f7b
Wenshan Ren <renws1990@gmail.com>
2012-06-10 21:27:24 +1000

    twitter-mode, better org2blog

--- init.el
+++ init.el
@@ -1,12 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
+;; start server, used for emacsclient
+(server-start)
 
 ;; ---- Chinese input ----
 (require 'ibus)
 (add-hook 'after-init-hook 'ibus-mode-on)
 (ibus-define-common-key ?\C-/ nil)
 (setq ibus-cursor-color '("red" "blue" "limegreen"))
-
 ;; ---- Chinese input end ---
+
 ;; ---- Org mode ---
 (setq load-path (cons "~/.emacs.d/site-lisp/org-mode/lisp" load-path))
 (setq load-path (cons "~/.emacs.d/site-lisp/org-mode/contrib/lisp" load-path))
@@ -24,7 +26,9 @@
          :username "wenshanren"
          :default-title "Hello World"
          :default-categories ("Hack" "Android")
-         :tags-as-categories nil)
+         :tags-as-categories nil
+         :wp-code nil
+         :keep-new-lines t)
         ))
 
 ;; ---- End Blog posting ---
@@ -39,8 +43,7 @@
 
 ;; Color theme
 (color-theme-initialize)
-(color-theme-matrix)
-;; (color-theme-subtle-hacker)
+(color-theme-subtle-hacker)
 
 ;; Don't show start up sceen
 (setq inhibit-startup-message t)
@@ -81,6 +84,20 @@
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 
+;; ido-mode
+(ido-mode 1)
+
+;; turn off tool bar, scroll bar and menu bar
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; copy-paste
+(setq x-select-enable-clipboard t)
+
+;; line width (fixed to 80)
+(setq-default fill-column 80)
+
 ;; ---- Nicer end ----
 
 
@@ -91,6 +108,7 @@
 ;;Don't map TAB to yasnippet
 ;;In fact, set it to something we'll never use because
 ;;we'll only ever trigger it indirectly.
+
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/site-lisp/yasnippet-0.5.9/snippets")
 
@@ -106,6 +124,7 @@
 (global-set-key [f4] 'flymake-goto-next-error)
 ;; == flymake ==
 
+;; == Python ==
 (progn (cd "~/.emacs.d/site-lisp")
 	(normal-top-level-add-subdirs-to-load-path))
 (require 'python)
@@ -163,7 +182,7 @@
   "Run pylookup-update and create the database at `pylookup-db-file'." t)
 (global-set-key "\C-ch" 'pylookup-lookup)
 
-;; == Python ==
+;; == END Python ==
 
 
 ;; == BEGIN Perl ==
@@ -224,6 +243,43 @@
 (eval-after-load 'pde-load
   '(add-hook 'cperl-mode-hook (lambda ()
                                 (outline-minor-mode 1))))
+
+(defun perl-eval () "Run selected region as Perl code" (interactive)
+   (shell-command-on-region (mark) (point) "perl "))
+(global-set-key (kbd "<f9>") 'perl-eval)
+
+
+(defun pde-perl-mode-hook ()
+   ;; chmod when saving
+  (when (and buffer-file-name
+        (not (string-match "\\.\\(pm\\|pod\\)$" (buffer-file-name))))
+      (add-hook 'after-save-hook 'executable-chmod nil t))
+  (set (make-local-variable 'compile-dwim-check-tools) nil))
+
+(defun flymake-display-current-error ()
+          "Display errors/warnings under cursor."
+      (interactive)
+      (let ((ovs (overlays-in (point) (1+ (point)))))
+        (catch 'found
+          (dolist (ov ovs)
+            (when (flymake-overlay-p ov)
+              (message (overlay-get ov 'help-echo))
+              (throw 'found t))))))
+(global-set-key (kbd "<f5>") 'flymake-display-current-error)
+
+    (defun flymake-goto-next-error-disp ()
+      "Go to next error in err ring, then display error/warning."
+      (interactive)
+      (flymake-goto-next-error)
+      (flymake-display-current-error))
+
+(global-set-key (kbd "<f8>") 'flymake-display-current-error)
+    (defun flymake-goto-prev-error-disp ()
+      "Go to previous error in err ring, then display error/warning."
+      (interactive)
+      (flymake-goto-prev-error)
+      (flymake-display-current-error))
+
 ;; == END Perl ==
 ;; ---- Programming end ----
 
@@ -242,3 +298,22 @@
 (global-set-key [(meta f3)] 'highlight-symbol-prev)
 
 ;; ---- Code Reading end ----
+
+
+;; ---- Microblog ---
+(add-to-list 'load-path "~/.emacs.d/site-lisp/twitter-mode")
+(require 'twittering-mode)
+(setq twittering-use-master-password t)
+(setq twittering-allow-insecure-server-cert t)
+(setq twittering-oauth-use-ssl nil)
+(setq twittering-use-ssl nil)
+(twittering-enable-unread-status-notifier)
+(setq-default twittering-icon-mode t)
+(setq twittering-initial-timeline-spec-string `(":home@sina"))
+;; ---- END Microblog
+
+
+;; ---- Email ----
+
+;; external editor of thunderbird
+(require 'tbemail)
---
+++ twitter-mode/README.markdown
@@ -0,0 +1,96 @@
+xwl's fork
+----------
+
+* Quick start:
+  http://www.cnblogs.com/william9/archive/2011/04/17/william9.html
+
+* Add login multiple services simultaneously support, e.g, both twitter and
+  sina. http://xwl.appspot.com/images/twitter-weibo-douban.png
+
+* New backends:
+  * http://www.socialcast.com
+  * http://www.douban.com
+  * http://t.sina.com.cn
+
+* (twitter) Add Stream API support.  To use,
+  * USERSTREAM: Go to `:home@twitter': M-x twittering-stream-mode
+  * STREAM: Go to `:public@twitter':   M-x twittering-stream-mode
+
+* unread tweets
+  * Show unread tweets for different buffers in a way similar to
+    erc-track-mode.
+  * Do not display unread notifier when updating tweets from a twittering
+    buffer.
+  * Show twitter logo on mode line.
+      Screenshot: http://xwl.appspot.com/images/twitter_unread.png
+  * Make unread notifier on mode line mouse clickable.
+  * Cache latest status for interested specs, so that next time when we restart
+    twittering-mode, unread count could be up-to-date.
+
+* misc
+  * Fetch replied statues on request.
+  * Format tweets sent by myself specially: Inspired by "Twitter for iPhone",
+    currently an ad-hoc implementation, screenshot:
+    http://xwl.appspot.com/images/twitter_my_tweet.png
+  * Render user profile: When visiting user timeline, we will place user profile
+    info near its most recent tweet.  Currently, including big image, basic, bio,
+    location, web, following status, lists, etc.  Screenshot:
+    http://xwl.appspot.com/images/twittering_user_profile.png
+  * Decorate tweets with zebra like background.
+    Screenshot: http://xwl.appspot.com/images/twitter.png
+  * (twittering-mode-setup): Short mode-name to just "twittering"
+  * Use defcustom to separate user customizable variables and internal
+    variables.
+
+ Twittering-mode: a Twitter client for Emacs
+=============================================
+
+Twittering-mode enables you to twit on Emacsen.
+
+- web: http://twmode.sf.net
+- github: http://github.com/hayamiz/twittering-mode
+
+ Features
+----------
+
+* Activities on Twitter
+  * Viewing various timelines
+    * Friends' timeline
+    * Replies
+    * User's timeline
+    * Public timeline
+  * Posting tweets
+    * Direct message
+    * ReTweet
+    * Hash tag
+    * Signiture
+  * Following and removing users
+  * Marking tweets as favorites
+* HTTP Proxy support
+* Secure connection via HTTPS (cURL is required)
+
+ Supported Emacsen
+-------------------
+
+- GNU Emacs 21 (some restrictions)
+- GNU Emacs 22, 23
+
+ Authors & Contributors
+------------------------
+
+- Y. Hayamizu
+- naoya_t
+- Tsuyoshi CHO
+- Alberto Garcia
+- Satoshi Yatagawa
+- 高山智也
+- 松尾(cvmat)
+- 青田(naota)
+- Jaemok Jeong(jmjeong)
+- Thomas Danckaert
+- IMAI Toshiyuki
+
+ See also
+----------
+
+- http://www.emacswiki.org/emacs-en/TwitteringMode
