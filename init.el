b25870d3b21f6663aa3fd2a2f1b5200d37783a0b
Wenshan <renws1990@gmail.com>
2013-03-21 19:52:45 +1100

    Added mozrepl for web development

--- init.el
+++ init.el
@@ -756,8 +756,32 @@
               (flymake-init-create-temp-buffer-copy
                'flymake-create-temp-inplace))))
 
-;; use common indentation (4 spaces)
-(setq sgml-basic-offset 4)
+;;; Mozilla REPL,
+(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
+
+(add-hook 'javascript-mode-hook 'javascript-custom-setup)
+(defun javascript-custom-setup ()
+  (moz-minor-mode 1))
+
+(require 'moz)
+(require 'json)
+
+(defun moz-update (&rest ignored)
+  "Update the remote mozrepl instance"
+  (interactive)
+  (comint-send-string (inferior-moz-process)
+    (concat "content.document.body.innerHTML="
+             (json-encode (buffer-string)) ";")))
+
+(defun moz-enable-auto-update ()
+  "Automatically the remote mozrepl when this buffer changes"
+  (interactive)
+  (add-hook 'after-change-functions 'moz-update t t))
+
+(defun moz-disable-auto-update ()
+  "Disable automatic mozrepl updates"
+  (interactive)
+  (remove-hook 'after-change-functions 'moz-update t))
 
 ;;----------------------------------------------------------
 ;; ---- END web development ----
