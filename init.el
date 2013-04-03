6191379c84a65663295b0d8626ef985199c56a27
Wenshan <renws1990@gmail.com>
2013-04-03 23:41:12 +1100

    Added quick Google search (code snippet is from Emacs Redux)

--- init.el
+++ init.el
@@ -286,6 +286,31 @@
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; find file as root
+(defun djcb-find-file-as-root ()
+  "Like `ido-find-file, but automatically edit the file with
+root-privileges (using tramp/sudo), if the file is not writable by
+user."
+  (interactive)
+  (let ((file (ido-read-file-name "Edit as root: ")))
+    (unless (file-writable-p file)
+      (setq file (concat "/sudo:root@localhost:" file)))
+    (find-file file)))
+(global-set-key (kbd "C-x F") 'djcb-find-file-as-root)
+
+;; Google search from Emacs
+(defun google ()
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
+
+(global-set-key (kbd "C-c g") 'google)
+
 ;;----------------------------------------------------------
 ;; ---- END nicer ----
 ;;----------------------------------------------------------
